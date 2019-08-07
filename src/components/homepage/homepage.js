894f8d597475dae165cbc59e9e6a5e793b94a782
Caridad Rivera <ccrive10@gmail.com>
2019-08-07 08:28:53 -0400

    up and running, background set

---
+++ src/components/homepage/homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.css'
+const Homepage = () => {
+
+    return (
+      <div className="homepage">
+      <h1>Hi </h1>
+      </div>
+    )
+
+}
+
+export default Homepage;
